6d43430ba64a081e6ce9806306c6c189becd1a83
Piero Lescano <71156605+piero-vic@users.noreply.github.com>
2021-12-12 16:52:07 -0500

    Remove some comments and add new git and cli aliases

--- zsh/.zshrc.d/git.zsh
+++ zsh/.zshrc.d/git.zsh
@@ -3,39 +3,32 @@
 
 alias g='git'
 
-# Git status alias
 alias gs='git status'
 
-# Git add and remove aliases
 alias ga='git add'
 alias gaa='git add --all'
+alias grs='git restore --staged'
+alias gr='git rm'
 
-# Git branch alias
 alias gb='git branch'
 alias gba='git branch -a'
 
-# Git remove alias
-alias gr='git rm'
-
-# Git move alias
 alias gmv='git mv'
 
-# Git commit aliases
 alias gc='git commit'
 alias gcm='git commit -m'
 
-# Git checkout aliases
 alias gco='git checkout'
 alias gcb='git checkout -b'
 
-# Git checkout aliases
 alias gl='git log --oneline'
 
-# Git push and  pull
-alias push='git push'
+alias gd='git diff'
+alias gds='git diff --staged'
+
 alias pull='git pull'
+alias push='git push'
 alias gpuo='git push -u origin'
 
-# Git stash
 alias gst='git stash'
-
+alias gstp='git stash pop'
