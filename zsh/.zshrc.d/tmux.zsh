b61559e7b80f1de25636e2a30877cc2db444564f
Piero Lescano <71156605+piero-vic@users.noreply.github.com>
2021-12-19 14:14:35 -0500

    tmux: Add new shell function and open tmux on every login shell.
    - Add new shell function to attach to sessions.
    - Configure .zshrc to open tmux on every login shell.
    - Minor styling to status bar.

--- zsh/.zshrc.d/tmux.zsh
+++ zsh/.zshrc.d/tmux.zsh
@@ -2,10 +2,15 @@
 #  █  █ ▀ █ ▀▄█ █ █
 
 alias t='tmux'
-alias ta='tmux attach -t'
-alias tas='tmux attach-session'
-alias tad='tmux attach -d -t'
-alias ts='tmux new-session -s'
+
+ta() {
+	if [ $# -eq 0 ]; then
+		tmux attach;
+	else
+		tmux attach -t "$@";
+	fi;
+}
+
 alias tl='tmux list-sessions'
 alias tksv='tmux kill-server'
 alias tkss='tmux kill-session -t'
