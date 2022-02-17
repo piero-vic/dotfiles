244b450fcb92c257a51a3358edac7ba696ababc2
Piero Lescano <71156605+piero-vic@users.noreply.github.com>
2022-02-16 23:16:54 -0500

    feat(neovim): Add protected calls in comment and treesitter

--- nvim/.config/nvim/lua/user/comment.lua
+++ nvim/.config/nvim/lua/user/comment.lua
@@ -1,4 +1,9 @@
-require'Comment'.setup {
+local status_ok, comment = pcall(require, "Comment")
+if not status_ok then
+  return
+end
+
+comment.setup {
   pre_hook = function(ctx)
     local U = require "Comment.utils"
 
--- nvim/.config/nvim/lua/user/treesitter.lua
+++ nvim/.config/nvim/lua/user/treesitter.lua
@@ -1,13 +1,18 @@
-require'nvim-treesitter.configs'.setup {
+local status_ok, treesitter = pcall(require, "nvim-treesitter.configs")
+if not status_ok then
+  return
+end
+
+treesitter.setup {
   ensure_installed = "maintained",
 
   sync_install = false,
-  
+
   highlight = {
     enable = true,
     additional_vim_regex_highlighting = false,
   },
-  
+
   context_commentstring = {
     enable = true,
     enable_autocmd = false,
