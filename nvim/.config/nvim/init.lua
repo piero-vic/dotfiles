374c341abf4a44affbd283023af21196880765a3
Piero Lescano <71156605+piero-vic@users.noreply.github.com>
2022-02-16 23:14:52 -0500

    feat(neovim): Add loop to init.lua

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,9 +1,18 @@
-require "user.options"
-require "user.keymaps"
-require "user.plugins"
-require "user.colorscheme"
-require "user.lsp"
-require "user.cmp"
-require "user.treesitter"
-require "user.comment"
-require "user.telescope"
+local sources = {
+  "user.options",
+  "user.keymaps",
+  "user.plugins",
+  "user.colorscheme",
+  "user.lsp",
+  "user.cmp",
+  "user.treesitter",
+  "user.comment",
+  "user.telescope"
+}
+
+for _, source in ipairs(sources) do
+  local status_ok, fault = pcall(require, source)
+  if not status_ok then
+    error("Failed to load " .. source .. "\n\n" .. fault)
+  end
+end
