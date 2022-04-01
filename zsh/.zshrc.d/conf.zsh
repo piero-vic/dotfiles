682d7fe36b0dca1aeb5b8160c927ab8d15994e84
Piero Lescano <71156605+piero-vic@users.noreply.github.com>
2022-04-01 14:06:17 -0500

    feat(zsh): Rewrite `.zshrc`
    - Delete unused aliases
    - Delete `.zshrc.d`
    - Add syntax highlighting

--- zsh/.zshrc.d/conf.zsh
+++
@@ -1,5 +0,0 @@
-alias conf-vim="nvim ~/.config/nvim/init.lua"
-alias conf-alacritty="nvim ~/.config/alacritty/alacritty.yml"
-alias conf-zsh="nvim ~/.zshrc ~/.zshrc.d/*"
-alias conf-tmux="nvim ~/.tmux.conf"
-alias conf-starship="nvim ~/.config/starship.toml"
