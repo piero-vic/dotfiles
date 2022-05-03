2fd87fe50e09b00c34ae3463d85ae27b5cdbfc36
Piero Lescano <pierolescano@protonmail.com>
2022-05-02 22:55:04 -0500

    refactor(arch): Add script for brightness notifications
    I also added `~/bin` to the PATH variable. This should be done before
    entering Sway so I put it on `.zshenv`

---
+++ arch/bin/brightness.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+device="${2:-intel_backlight}"
+
+get_brightness () {
+	brightnessctl -d "$device" | tr ' ' '\n' | grep '%' | tr -d '()'
+}
+
+inc_brightness () {
+  brightnessctl -d "$device" -q set +5%
+	get_brightness
+}
+
+dec_brightness () {
+  brightnessctl -d "$device" -q set 5%-
+	get_brightness
+}
+
+show_help () {
+	cat <<EOF
+Usage: brightness.sh [options] [device]
+AVAILABLE OPTIONS:
+  --get         get current brightness
+  --inc         increase current brightness by +5
+  --dec         decrease current brightness by -5
+EOF
+}
+
+if [[ "$1" == "--get" ]]; then
+	get_brightness
+elif [[ "$1" == "--inc" ]]; then
+	inc_brightness
+elif [[ "$1" == "--dec" ]]; then
+	dec_brightness
+else
+	show_help
+fi
