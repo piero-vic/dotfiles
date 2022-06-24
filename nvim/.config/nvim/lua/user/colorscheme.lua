03fe5aa08b4b04c9692e7592121e0ef003eeadb7
Piero Lescano <pierolescano@protonmail.com>
2022-06-23 21:37:18 -0500

    feat(arch): Add flavours and update colors

--- nvim/.config/nvim/lua/user/colorscheme.lua
+++ nvim/.config/nvim/lua/user/colorscheme.lua
@@ -5,6 +5,7 @@ function M.config()
 
   vim.g.tokyonight_style = 'night'
   vim.g.tokyonight_italic_functions = true
+  vim.g.tokyonight_transparent = true
 
   local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
   if not status_ok then
