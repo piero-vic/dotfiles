7ca25bef6564308880748c699c7a49cafa096f17
Piero Lescano <pierolescano@protonmail.com>
2022-08-14 01:12:28 -0500

    refactor(arch): Rewrite brightness.sh

--- arch/bin/brightness.sh
+++ arch/bin/brightness.sh
@@ -6,32 +6,35 @@ get_brightness() {
   brightnessctl -d "$device" | tr ' ' '\n' | grep '%' | tr -d '()'
 }
 
+notify() {
+  if [[ "$device" = 'intel_backlight' ]]; then
+    notify-send " Screen" "Brightness: $(get_brightness)"
+  elif [[ "$device" = 'smc::kbd_backlight' ]]; then
+    notify-send " Keyboard" "Brightness: $(get_brightness)"
+  fi
+}
+
 inc_brightness() {
-  brightnessctl -d "$device" -q set +5%
-  get_brightness
+  brightnessctl -d "$device" -q set +5% && notify
 }
 
 dec_brightness() {
-  brightnessctl -d "$device" -q set 5%-
-  get_brightness
+  brightnessctl -d "$device" -q set 5%- && notify
 }
 
 show_help() {
   cat <<EOF
 Usage: brightness.sh [options] [device]
 AVAILABLE OPTIONS:
-  --get         get current brightness
-  --inc         increase current brightness by +5
-  --dec         decrease current brightness by -5
+  --inc         increase brightness by +5%
+  --dec         decrease brightness by -5%
 EOF
 }
 
-if [[ "$1" == "--get" ]]; then
-	get_brightness
-elif [[ "$1" == "--inc" ]]; then
-	inc_brightness
+if [[ "$1" == "--inc" ]]; then
+  inc_brightness
 elif [[ "$1" == "--dec" ]]; then
-	dec_brightness
+  dec_brightness
 else
-	show_help
+  show_help
 fi
