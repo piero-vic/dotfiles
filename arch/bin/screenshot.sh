7669c88fc404c08a47c5f767d352d2990fab56c0
Piero Lescano <pierolescano@protonmail.com>
2022-08-14 01:14:40 -0500

    chore(arch): Format scripts

--- arch/bin/screenshot.sh
+++ arch/bin/screenshot.sh
@@ -6,29 +6,29 @@ filename="$screenshots_directory/$(date '+%Y%m%d_%Hh%Mm%Ss')_grim.png"
 [[ -d $screenshots_directory ]] || mkdir -p $screenshots_directory
 
 send_notification() {
-	notify-send -i "$filename" " Grim" "Screenshot saved"
+  notify-send -i "$filename" " Grim" "Screenshot saved"
 }
 
 get_screen() {
-	grim "$filename" && send_notification
+  grim "$filename" && send_notification
 }
 
 get_window() {
-	grim -g "$(swaymsg -t get_tree | jq -r '.. | select(.focused?) | .rect | "\(.x),\(.y) \(.width)x\(.height)"')" "$filename" &&
-		send_notification
+  grim -g "$(swaymsg -t get_tree | jq -r '.. | select(.focused?) | .rect | "\(.x),\(.y) \(.width)x\(.height)"')" "$filename" &&
+    send_notification
 }
 
 get_section() {
-	slurp_output="$(slurp 2>&1)"
-	if [[ "$slurp_output" == "selection cancelled" ]]; then
-		notify-send " Grim" "Selection cancelled"
-	else
-		grim -g "$slurp_output" "$filename" && send_notification
-	fi
+  slurp_output="$(slurp 2>&1)"
+  if [[ "$slurp_output" == "selection cancelled" ]]; then
+    notify-send " Grim" "Selection cancelled"
+  else
+    grim -g "$slurp_output" "$filename" && send_notification
+  fi
 }
 
 show_help() {
-	cat <<EOF
+  cat <<EOF
 Usage: screenshot.sh [options]
 AVAILABLE OPTIONS:
   --screen           grab the whole screen
@@ -38,11 +38,11 @@ EOF
 }
 
 if [[ "$1" == "--screen" ]]; then
-	get_screen
+  get_screen
 elif [[ "$1" == "--window" ]]; then
-	get_window
+  get_window
 elif [[ "$1" == "--section" ]]; then
-	get_section
+  get_section
 else
-	show_help
+  show_help
 fi
