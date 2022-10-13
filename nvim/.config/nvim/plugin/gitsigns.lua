ecb8610f390ee14393fd34d3c0edfd6a5ad6985f
Piero Lescano <pierolescano@protonmail.com>
2022-10-12 20:34:58 -0500

    feat(neovim): Add gitsigns keymaps

--- nvim/.config/nvim/plugin/gitsigns.lua
+++ nvim/.config/nvim/plugin/gitsigns.lua
@@ -1,1 +1,22 @@
-require('gitsigns').setup()
+-- ╔═══════════════════════════════════════════════════════════════════════════╗
+-- ║ Gitsigns                                                                  ║
+-- ╚═══════════════════════════════════════════════════════════════════════════╝
+
+require('gitsigns').setup {
+  on_attach = function(bufnr)
+    local gs = package.loaded.gitsigns
+
+    local function map(mode, l, r, opts)
+      opts = opts or { noremap = true, silent = true }
+      opts.buffer = bufnr
+      vim.keymap.set(mode, l, r, opts)
+    end
+
+    map({'n', 'v'}, '<leader>hs', ':Gitsigns stage_hunk<CR>')
+    map({'n', 'v'}, '<leader>hr', ':Gitsigns reset_hunk<CR>')
+    map('n', '<leader>hu', gs.undo_stage_hunk)
+    map('n', '<leader>hS', gs.stage_buffer)
+    map('n', '<leader>hR', gs.reset_buffer)
+    map('n', '<leader>hp', gs.preview_hunk)
+  end,
+}
