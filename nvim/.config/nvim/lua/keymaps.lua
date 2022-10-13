0b8b23c6edaa4ff808c77b84ee54703c8eee59f1
Piero Lescano <pierolescano@protonmail.com>
2022-10-12 20:37:02 -0500

    chore(neovim): Organize keymaps

--- nvim/.config/nvim/lua/keymaps.lua
+++ nvim/.config/nvim/lua/keymaps.lua
@@ -1,21 +1,20 @@
 local opts = { noremap = true, silent = true }
 local keymap = vim.keymap.set
 
+-- Leader key
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 keymap({ 'n', 'v' }, '<Space>', '<Nop>', opts)
+
+-- Clear search highlighting
+keymap('n', '<leader>c', ':nohlsearch<CR>', opts)
 
 -- Navigate buffers
 keymap('n', '<TAB>', ':bnext<CR>', opts)
 keymap('n', '<S-TAB>', ':bprevious<CR>', opts)
 
--- Open file explorer
-keymap('n', '<C-N>', ':Lex 15<CR>', opts)
-
--- Consecutive tabs on visual mode
-keymap('v', '<', '<gv', opts)
-keymap('v', '>', '>gv', opts)
-
--- Move text up and down
-keymap('x', 'J', ":move '>+1<CR>gv-gv", opts)
+-- Move text in visual mode
 keymap('x', 'K', ":move '<-2<CR>gv-gv", opts)
+keymap('x', 'J', ":move '>+1<CR>gv-gv", opts)
+keymap('v', '>', '>gv', opts)
+keymap('v', '<', '<gv', opts)
