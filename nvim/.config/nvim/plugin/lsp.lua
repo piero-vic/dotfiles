313d11d76cea5bca0ed905a75081df9ff5c60907
Piero Lescano <pierolescano@protonmail.com>
2022-10-28 20:33:34 -0500

    chore(neovim): Organize plugins
    Remove markdown-preview
    Change update_capabilities to default_capabilities in cmp_nvim_lsp

--- nvim/.config/nvim/plugin/lsp.lua
+++ nvim/.config/nvim/plugin/lsp.lua
@@ -35,7 +35,7 @@ local on_attach = function(_, bufnr)
   nmap('<leader>k', vim.lsp.buf.signature_help)
 end
 
-local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+local capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities())
 
 local servers = {
   'astro',
