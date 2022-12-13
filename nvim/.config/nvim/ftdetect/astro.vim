116b4f0b05eef3e71d85aaf5671ad95a448b932c
Piero Lescano <71156605+piero-vic@users.noreply.github.com>
2022-12-12 23:57:45 -0500

    refactor(neovim): Use autocmds for handle astro and ledger files

--- nvim/.config/nvim/ftdetect/astro.vim
+++
@@ -1,1 +0,0 @@
-au BufNewFile,BufRead *.astro set filetype=astro
--- nvim/.config/nvim/lua/autocmds.lua
+++ nvim/.config/nvim/lua/autocmds.lua
@@ -1,3 +1,18 @@
+-- Filetypes
+vim.api.nvim_create_autocmd({'BufNewFile', 'BufRead'}, {
+  pattern = '*.astro',
+  command = 'set filetype=astro',
+})
+
+-- Ledger files
+vim.api.nvim_create_autocmd({'BufNewFile', 'BufRead'}, {
+  pattern = '*.ledger',
+  callback = function()
+    vim.opt_local.shiftwidth = 4
+    vim.opt_local.commentstring = ';%s'
+  end,
+})
+
 -- Code formating on save
 vim.api.nvim_create_autocmd('BufWritePre', {
   pattern = '*.go',
