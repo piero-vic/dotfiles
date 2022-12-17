b67ca1d005ced4bf525e56abfe65e614d76fd948
Piero Lescano <71156605+piero-vic@users.noreply.github.com>
2022-12-17 11:08:00 -0500

    feat(sway): Add countdown and delete options in screenrecorder

--- sway/.local/bin/screenrecord.sh
+++ sway/.local/bin/screenrecord.sh
@@ -7,42 +7,10 @@ recordings_directory=~/videos/screenrecordings
 filename="$recordings_directory/$(date '+%Y%m%d_%Hh%Mm%Ss')_recording.mp4"
 [[ -d $recordings_directory ]] || mkdir -p $recordings_directory
 
-# Options
-record_screen() {
-  notify-send -a screenrecorder -t 3000 ' Screen recording' "Screen recording was started!"
-  sleep 3
-  wf-recorder -f "$filename" >/dev/null 2>&1 &
-  echo -n "$output_rec"
-  exit 0
-}
-
-record_window() {
-  notify-send -a screenrecorder -t 3000 ' Screen recording' "Screen recording was started!"
-  sleep 3
-  wf-recorder -f "$filename" -g "$(swaymsg -t get_tree | jq -r '.. | select(.focused?) | .rect | "\(.x),\(.y) \(.width)x\(.height)"')" >/dev/null 2>&1 &
-  echo -n "$output_rec"
-  exit 0
-}
-
-record_section() {
-  slurp_output="$(slurp 2>&1)"
-  if [[ "$slurp_output" == "selection cancelled" ]]; then
-    notify-send " Screen recording" "Selection cancelled"
-  else
-    grim -g "$slurp_output" "$filename" && send_notification
-    notify-send -a screenrecorder -t 3000 ' Screen recording' "Screen recording was started!"
-    sleep 3
-    wf-recorder -f "$filename" -g "$slurp_output" >/dev/null 2>&1 &
-    echo -n "$output_rec"
-    exit 0
-  fi
-}
-
-# Run
 if pidof wf-recorder >/dev/null 2>&1; then
   if [[ "$1" == "toggle" ]]; then
     killall -s SIGINT wf-recorder >/dev/null 2>&1
-    notify-send -a screenrecorder -t 3000 ' Screen recording' "Screen recording was stopped!"
+    notify-send -a screenrecorder -t 1000 ' Screen recording' "Screen recording was stopped!"
     echo -n "$output_off"
     exit 0
   fi
@@ -50,23 +18,20 @@ if pidof wf-recorder >/dev/null 2>&1; then
   exit 0
 else
   if [[ "$1" == "toggle" ]]; then
-    case $(
-      bemenu --no-exec --prompt="screenrecord" <<EOF | sed 's/^ *//'
-(S)creen
-(W)indow
-(X)Section
-EOF
-    ) in
-    "(S)creen")
-      record_screen
-      ;;
-    "(W)indow")
-      record_window
-      ;;
-    "(X)Section")
-      record_section
-      ;;
-    esac
+    count=5
+
+    while [[ $count -gt 1 ]]; do
+      notify-send -a screenrecorder -t 1100 ' Screen recording' "Screen recording stars in $count"
+      ((count -= 1 ))
+      sleep 1
+    done
+
+    notify-send -a screenrecorder -t 1000 ' Screen recording' "Screen recording stars in 1"
+    sleep 1
+
+    wf-recorder -f "$filename" >/dev/null 2>&1 &
+    echo -n "$output_rec"
+    exit 0
   fi
   echo -n "$output_off"
   exit 0
