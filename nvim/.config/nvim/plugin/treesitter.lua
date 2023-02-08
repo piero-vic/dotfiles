ea5fc5c7330c8fb7b0bcd747e2818c0cecc365b3
Piero Lescano <71156605+piero-vic@users.noreply.github.com>
2023-02-07 21:00:43 -0500

    refactor(nvim): Organize config

--- nvim/.config/nvim/plugin/treesitter.lua
+++
@@ -1,6 +0,0 @@
-require('nvim-treesitter.configs').setup {
-  ensure_installed = 'all',
-  sync_install = false,
-  highlight = { enable = true },
-  context_commentstring = { enable = true, enable_autocmd = false },
-}
