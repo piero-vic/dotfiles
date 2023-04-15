4827b298d5b7194f47bb794230e15f4258e85fc4
Piero Lescano <pierolescano@protonmail.com>
2023-04-15 14:36:39 -0500

    feat(nvim): Add hunk navigation with gitsigns

--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -29,11 +29,33 @@ return {
           vim.keymap.set(mode, l, r, opts)
         end
 
+        -- Navigation
+        map('n', ']h', function()
+          if vim.wo.diff then
+            return ']c'
+          end
+          vim.schedule(function()
+            gs.next_hunk()
+          end)
+          return '<Ignore>'
+        end, { expr = true })
+
+        map('n', '[h', function()
+          if vim.wo.diff then
+            return '[c'
+          end
+          vim.schedule(function()
+            gs.prev_hunk()
+          end)
+          return '<Ignore>'
+        end, { expr = true })
+
+        -- Actions
         map({ 'n', 'v' }, '<leader>hs', ':Gitsigns stage_hunk<CR>')
         map({ 'n', 'v' }, '<leader>hr', ':Gitsigns reset_hunk<CR>')
         map('n', '<leader>hu', gs.undo_stage_hunk)
-        map('n', '<leader>hS', gs.stage_buffer)
-        map('n', '<leader>hR', gs.reset_buffer)
+        map('n', '<leader>bs', gs.stage_buffer)
+        map('n', '<leader>br', gs.reset_buffer)
         map('n', '<leader>hp', gs.preview_hunk)
       end,
     },
