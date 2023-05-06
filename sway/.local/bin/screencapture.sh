17349381ce1ef397fffe695e9eb1470887f3209c
Piero Lescano <71156605+piero-vic@users.noreply.github.com>
2023-05-06 09:20:08 -0500

    fix: Update broken icons

--- sway/.local/bin/screencapture.sh
+++ sway/.local/bin/screencapture.sh
@@ -6,7 +6,7 @@ filename="$screenshots_directory/$(date '+%Y%m%d_%Hh%Mm%Ss')_grim.png"
 [[ -d $screenshots_directory ]] || mkdir -p $screenshots_directory
 
 send_notification() {
-  notify-send -i "$filename" " Grim" "Screenshot saved"
+  notify-send -i "$filename" "󰄀 Grim" "Screenshot saved"
 }
 
 get_screen() {
@@ -21,7 +21,7 @@ get_window() {
 get_section() {
   slurp_output="$(slurp 2>&1)"
   if [[ "$slurp_output" == "selection cancelled" ]]; then
-    notify-send " Grim" "Selection cancelled"
+    notify-send "󰄀 Grim" "Selection cancelled"
   else
     grim -g "$slurp_output" "$filename" && send_notification
   fi
