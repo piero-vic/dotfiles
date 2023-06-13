8f77f0177a684affd618778e2f2a4126efd4aa7c
Piero Lescano <71156605+piero-vic@users.noreply.github.com>
2023-06-12 23:28:21 -0500

    chore: Format scripts

--- sway/.local/bin/battery-notification.sh
+++ sway/.local/bin/battery-notification.sh
@@ -1,13 +1,20 @@
 #!/usr/bin/env bash
 
+script_name=${0}
+current_pid=$$
+previous_pid=$(ps -ef | grep "bash $script_name" | awk 'NR==1{print $2}')
+
+if [[ "$current_pid" != "$previous_pid" ]]; then
+	kill -9 $previous_pid
+fi
+
 while [[ 0 -eq 0 ]]; do
-  battery_status="$(cat /sys/class/power_supply/BAT0/status)"
-  battery_charge=$(acpi -b | grep -P -o '[0-9]+(?=%)')
-  echo $battery_status \| $battery_charge \| $(date '+%X')
+	battery_status="$(cat /sys/class/power_supply/BAT0/status)"
+	battery_charge=$(acpi -b | grep -P -o '[0-9]+(?=%)')
 
-  if [[ $battery_status == 'Discharging' && $battery_charge -le 15 ]]; then
-    notify-send -u critical -t 31000 "Low Battery!" "Battery level is ${battery_charge}%"
-  fi
+	if [[ $battery_status == 'Discharging' && $battery_charge -le 15 ]]; then
+		notify-send -u critical -t 31000 "Low Battery!" "Battery level is ${battery_charge}%"
+	fi
 
-  sleep 30
+	sleep 30
 done
--- sway/.local/bin/screencapture.sh
+++ sway/.local/bin/screencapture.sh
@@ -6,29 +6,29 @@ filename="$screenshots_directory/$(date '+%Y%m%d_%Hh%Mm%Ss')_grim.png"
 [[ -d $screenshots_directory ]] || mkdir -p $screenshots_directory
 
 send_notification() {
-  notify-send -i "$filename" "󰄀 Grim" "Screenshot saved"
+	notify-send -i "$filename" "󰄀 Grim" "Screenshot saved"
 }
 
 get_screen() {
-  grim "$filename" && send_notification
+	grim "$filename" && send_notification
 }
 
 get_window() {
-  grim -g "$(swaymsg -t get_tree | jq -r '.. | select(.focused?) | .rect | "\(.x),\(.y) \(.width)x\(.height)"')" "$filename" &&
-    send_notification
+	grim -g "$(swaymsg -t get_tree | jq -r '.. | select(.focused?) | .rect | "\(.x),\(.y) \(.width)x\(.height)"')" "$filename" &&
+		send_notification
 }
 
 get_section() {
-  slurp_output="$(slurp 2>&1)"
-  if [[ "$slurp_output" == "selection cancelled" ]]; then
-    notify-send "󰄀 Grim" "Selection cancelled"
-  else
-    grim -g "$slurp_output" "$filename" && send_notification
-  fi
+	slurp_output="$(slurp 2>&1)"
+	if [[ "$slurp_output" == "selection cancelled" ]]; then
+		notify-send "󰄀 Grim" "Selection cancelled"
+	else
+		grim -g "$slurp_output" "$filename" && send_notification
+	fi
 }
 
 case $(
-  bemenu --no-exec --prompt="screencapture" <<EOF | sed 's/^ *//'
+	bemenu --no-exec --prompt="screencapture" <<EOF
 Screen
 Window
 Selection
@@ -36,15 +36,15 @@ Record
 EOF
 ) in
 "Screen")
-  get_screen
-  ;;
+	get_screen
+	;;
 "Window")
-  get_window
-  ;;
+	get_window
+	;;
 "Selection")
-  get_section
-  ;;
+	get_section
+	;;
 "Record")
-  screenrecord.sh toggle
-  ;;
+	screenrecord.sh toggle
+	;;
 esac
