b6d093c286ac8e51b0fceb86f09c1b9764bb7b82
Piero Lescano <71156605+piero-vic@users.noreply.github.com>
2023-07-29 08:57:08 -0500

    chore(nvim): Remove LSP UI config

--- nvim/.config/nvim/lua/config/lsp/init.lua
+++ nvim/.config/nvim/lua/config/lsp/init.lua
@@ -67,15 +67,3 @@ lsp.setup_nvim_cmp {
 }
 
 lsp.setup()
-
--- ╔═══════════════════════════════════════════════════════════════════════════╗
--- ║ LSP UI Configuration                                                      ║
--- ╚═══════════════════════════════════════════════════════════════════════════╝
-
-require('mason.settings').set { ui = { border = 'single' } }
-require('lspconfig.ui.windows').default_options.border = 'single'
-
-vim.api.nvim_set_hl(0, 'LspInfoBorder', { link = 'FloatBorder' })
-vim.diagnostic.config { virtual_text = false, float = { border = 'single' } }
-vim.lsp.handlers['textDocument/hover'] = vim.lsp.with(vim.lsp.handlers.hover, { border = 'single' })
-vim.lsp.handlers['textDocument/signatureHelp'] = vim.lsp.with(vim.lsp.handlers.signature_help, { border = 'single' })
