e5e92e87cf106455178265382e78543541df4ddc
Piero Lescano <71156605+piero-vic@users.noreply.github.com>
2023-11-23 21:05:51 -0500

    feat(sway): Add lock file mechanism to battery-notification script

--- sway/.local/bin/battery-notification.sh
+++ sway/.local/bin/battery-notification.sh
@@ -1,16 +1,26 @@
 #!/usr/bin/env bash
 
-script_name=${0}
-current_pid=$$
-previous_pid=$(ps -ef | grep "bash $script_name" | awk 'NR==1{print $2}')
+set -e
 
-if [[ "$current_pid" != "$previous_pid" ]]; then
-	kill -9 $previous_pid
+lock_file="/tmp/battery_monitor.lock"
+
+if [[ -e "$lock_file" ]]; then
+	echo "Another instance is already running. Exiting."
+	exit 1
 fi
 
-while [[ 0 -eq 0 ]]; do
-	battery_status="$(cat /sys/class/power_supply/BAT0/status)"
-	battery_charge=$(acpi -b | grep -P -o '[0-9]+(?=%)')
+touch "$lock_file"
+
+remove_lock_file() {
+	rm -f "$lock_file"
+	exit 1
+}
+
+trap 'remove_lock_file' EXIT SIGINT SIGTERM
+
+while true; do
+	battery_status=$(cat /sys/class/power_supply/BAT0/status)
+	battery_charge=$(cat /sys/class/power_supply/BAT0/capacity)
 
 	if [[ $battery_status == 'Discharging' && $battery_charge -le 15 ]]; then
 		notify-send -u critical -t 31000 "Low Battery!" "Battery level is ${battery_charge}%"
