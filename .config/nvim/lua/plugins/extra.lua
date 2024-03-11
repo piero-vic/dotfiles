5c4f81bba98610d17f5487bec0f5dde7d6e7f1ee
Piero Lescano <71156605+piero-vic@users.noreply.github.com>
2024-03-10 23:58:24 -0500

    feat(nvim): Replace vim-tmux-navigator for nvim-tmux-navigation

--- .config/nvim/lua/plugins/extra.lua
+++ .config/nvim/lua/plugins/extra.lua
@@ -1,6 +1,20 @@
 return {
   -- Tmux
-  'christoomey/vim-tmux-navigator',
+  {
+    'alexghergh/nvim-tmux-navigation',
+    opts = { disable_when_zoomed = true },
+    config = function(_, opts)
+      local nvim_tmux_nav = require 'nvim-tmux-navigation'
+
+      nvim_tmux_nav.setup(opts)
+
+      vim.keymap.set('n', '<C-h>', nvim_tmux_nav.NvimTmuxNavigateLeft)
+      vim.keymap.set('n', '<C-j>', nvim_tmux_nav.NvimTmuxNavigateDown)
+      vim.keymap.set('n', '<C-k>', nvim_tmux_nav.NvimTmuxNavigateUp)
+      vim.keymap.set('n', '<C-l>', nvim_tmux_nav.NvimTmuxNavigateRight)
+    end,
+    keys = { '<C-h>', '<C-j>', '<C-k>', '<C-l>' },
+  },
 
   -- Ledger
   'ledger/vim-ledger',
