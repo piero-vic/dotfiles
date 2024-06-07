17f803ed95f058dcd58a9c5eaf002e8c85268cd9
Piero Lescano <71156605+piero-vic@users.noreply.github.com>
2024-06-07 17:58:02 -0500

    feat(neovim): Add todo-comments.nvim

--- .config/nvim/lua/plugins/ui.lua
+++ .config/nvim/lua/plugins/ui.lua
@@ -82,6 +82,14 @@ return {
     },
   },
 
+  -- Annotated comments
+  {
+    'folke/todo-comments.nvim',
+    event = 'VimEnter',
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    opts = { signs = false },
+  },
+
   -- Cloak
   { 'laytan/cloak.nvim', opts = {} },
 }
