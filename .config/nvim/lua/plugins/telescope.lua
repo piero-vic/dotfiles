fd76614739abdbd9fd0216a2bdc3656c0b663404
Piero Lescano <71156605+piero-vic@users.noreply.github.com>
2024-09-20 22:34:45 -0500

    feat(neovim): Allow telescope to search symbolic links

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -5,13 +5,24 @@ return {
     'nvim-telescope/telescope-ui-select.nvim',
   },
   config = function()
-    require('telescope').setup {
+    local telescope = require 'telescope'
+    local config = require 'telescope.config'
+    local builtin = require 'telescope.builtin'
+
+    local vimgrep_arguments = { unpack(config.values.vimgrep_arguments) }
+    table.insert(vimgrep_arguments, '--hidden')
+    table.insert(vimgrep_arguments, '--glob')
+    table.insert(vimgrep_arguments, '!**/.git/*')
+    table.insert(vimgrep_arguments, '-L')
+
+    telescope.setup {
       defaults = {
         sorting_strategy = 'ascending',
+        vimgrep_arguments = vimgrep_arguments,
       },
       pickers = {
         find_files = {
-          find_command = { 'rg', '--files', '--hidden', '-g', '!.git' },
+          find_command = { 'rg', '--files', '--hidden', '--glob', '!**/.git/*', '-L' },
         },
       },
       extensions = {
@@ -23,10 +34,21 @@ return {
 
     pcall(require('telescope').load_extension, 'ui-select')
 
-    -- Telescope
-    vim.keymap.set('n', '<leader>fw', require('telescope.builtin').live_grep)
-    vim.keymap.set('n', '<leader>ff', require('telescope.builtin').find_files)
-    vim.keymap.set('n', '<leader>gl', ':Telescope git_commits initial_mode=normal<CR>')
-    vim.keymap.set('n', '<leader>gs', ':Telescope git_status initial_mode=normal<CR>')
+    -- General
+    vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Search Help' })
+    vim.keymap.set('n', '<leader>fk', builtin.keymaps, { desc = 'Search Keymaps' })
+    vim.keymap.set('n', '<leader>fw', builtin.live_grep, { desc = 'Search by Grep' })
+    vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Search Files' })
+    vim.keymap.set('n', '<leader>fd', builtin.diagnostics, { desc = 'Search Diagnostics' })
+    vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Search Buffer' })
+
+    -- Git
+    vim.keymap.set('n', '<leader>gl', function()
+      builtin.git_commits { initial_mode = 'normal' }
+    end, { desc = 'Git Log ' })
+
+    vim.keymap.set('n', '<leader>gs', function()
+      builtin.git_status { initial_mode = 'normal' }
+    end, { desc = 'Git Status' })
   end,
 }
