c1c9523d571eaee62c73b6e2e29ab4fbec5f98fe
Piero Lescano <me@pierolescano.com>
2025-01-09 20:20:40 -0500

    chore(neovim): Update gitsigns and telescope configuration

--- .config/nvim/lua/plugins/git.lua
+++ .config/nvim/lua/plugins/git.lua
@@ -1,18 +1,26 @@
 return {
   'lewis6991/gitsigns.nvim',
+  event = 'BufReadPre',
   opts = {
     on_attach = function(bufnr)
+      local gitsigns = require 'gitsigns'
+
       -- Navigation
-      vim.keymap.set('n', '[h', require('gitsigns').prev_hunk, { buffer = bufnr })
-      vim.keymap.set('n', ']h', require('gitsigns').next_hunk, { buffer = bufnr })
+      vim.keymap.set('n', '[h', function()
+        gitsigns.nav_hunk 'prev'
+      end, { buffer = bufnr, desc = 'Previous [H]unk' })
+      vim.keymap.set('n', ']h', function()
+        gitsigns.nav_hunk 'next'
+      end, { buffer = bufnr, desc = 'Next [H]unk' })
+
       -- Actions
-      vim.keymap.set({ 'n', 'v' }, '<leader>hs', require('gitsigns').stage_hunk, { buffer = bufnr })
-      vim.keymap.set({ 'n', 'v' }, '<leader>hr', require('gitsigns').reset_hunk, { buffer = bufnr })
-      vim.keymap.set('n', '<leader>hu', require('gitsigns').undo_stage_hunk, { buffer = bufnr })
-      vim.keymap.set('n', '<leader>bs', require('gitsigns').stage_buffer, { buffer = bufnr })
-      vim.keymap.set('n', '<leader>br', require('gitsigns').reset_buffer, { buffer = bufnr })
-      vim.keymap.set('n', '<leader>hp', require('gitsigns').preview_hunk, { buffer = bufnr })
-      vim.keymap.set('n', '<leader>gb', require('gitsigns').blame, { buffer = bufnr })
+      vim.keymap.set({ 'n', 'v' }, '<leader>hs', gitsigns.stage_hunk, { buffer = bufnr })
+      vim.keymap.set({ 'n', 'v' }, '<leader>hr', gitsigns.reset_hunk, { buffer = bufnr })
+      vim.keymap.set('n', '<leader>hu', gitsigns.undo_stage_hunk, { buffer = bufnr })
+      vim.keymap.set('n', '<leader>bs', gitsigns.stage_buffer, { buffer = bufnr })
+      vim.keymap.set('n', '<leader>br', gitsigns.reset_buffer, { buffer = bufnr })
+      vim.keymap.set('n', '<leader>hp', gitsigns.preview_hunk, { buffer = bufnr })
+      vim.keymap.set('n', '<leader>gb', gitsigns.blame, { buffer = bufnr })
     end,
   },
 }
