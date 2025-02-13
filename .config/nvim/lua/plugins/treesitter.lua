9131bd8a3ab93f0bc2fc5a0311896f5109265f4f
Piero Lescano <me@pierolescano.com>
2025-02-12 22:11:48 -0500

    chore(neovim): Remove Comment.nvim
    Neovim already has support for commenting.

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -40,15 +40,15 @@ return {
 
   -- Comments
   {
-    'numToStr/Comment.nvim',
-    dependencies = {
-      'JoosepAlviste/nvim-ts-context-commentstring',
-      opts = { enable_autocmd = false },
-    },
-    config = function()
-      require('Comment').setup {
-        pre_hook = require('ts_context_commentstring.integrations.comment_nvim').create_pre_hook(),
-      }
+    'JoosepAlviste/nvim-ts-context-commentstring',
+    opts = { enable_autocmd = true },
+    config = function(_, opts)
+      require('ts_context_commentstring').setup(opts)
+
+      local get_option = vim.filetype.get_option
+      vim.filetype.get_option = function(filetype, option)
+        return option == 'commentstring' and require('ts_context_commentstring.internal').calculate_commentstring() or get_option(filetype, option)
+      end
     end,
   },
 }
