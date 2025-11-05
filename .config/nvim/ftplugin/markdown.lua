d5f33d5b79e1eeeedacd8ba79d81cd38fcce01dd
Piero Lescano <me@pierolescano.com>
2025-11-05 08:16:11 -0500

    fix(neovim): Only set markdown keymaps in markdown buffers

--- .config/nvim/ftplugin/markdown.lua
+++ .config/nvim/ftplugin/markdown.lua
@@ -1,19 +1,19 @@
 -- strike through, bold, and italic shortcuts
-vim.keymap.set('v', '<localleader>s', 'c~~<c-r>"~~')
-vim.keymap.set('v', '<localleader>b', 'c**<c-r>"**')
-vim.keymap.set('v', '<localleader>i', 'c_<c-r>"_')
+vim.keymap.set('v', '<localleader>s', 'c~~<c-r>"~~', { buffer = 0 })
+vim.keymap.set('v', '<localleader>b', 'c**<c-r>"**', { buffer = 0 })
+vim.keymap.set('v', '<localleader>i', 'c_<c-r>"_', { buffer = 0 })
 
-vim.keymap.set('n', '<localleader>s', 'viwc~~<c-r>"~~<esc>')
-vim.keymap.set('n', '<localleader>b', 'viwc**<c-r>"**<esc>')
-vim.keymap.set('n', '<localleader>i', 'viwc_<c-r>"_<esc>')
+vim.keymap.set('n', '<localleader>s', 'viwc~~<c-r>"~~<esc>', { buffer = 0 })
+vim.keymap.set('n', '<localleader>b', 'viwc**<c-r>"**<esc>', { buffer = 0 })
+vim.keymap.set('n', '<localleader>i', 'viwc_<c-r>"_<esc>', { buffer = 0 })
 
 -- urls and titles
-vim.keymap.set('v', '<localleader>t', 'c[<c-r>"]()<left>')
-vim.keymap.set('v', '<localleader>u', 'c[](<c-r>")<c-o>F]')
+vim.keymap.set('v', '<localleader>t', 'c[<c-r>"]()<left>', { buffer = 0 })
+vim.keymap.set('v', '<localleader>u', 'c[](<c-r>")<c-o>F]', { buffer = 0 })
 
-vim.keymap.set('n', '<localleader>t', 'viwc[<c-r>"]()<left>')
-vim.keymap.set('n', '<localleader>u', 'viwc[](<c-r>")<c-o>F]')
+vim.keymap.set('n', '<localleader>t', 'viwc[<c-r>"]()<left>', { buffer = 0 })
+vim.keymap.set('n', '<localleader>u', 'viwc[](<c-r>")<c-o>F]', { buffer = 0 })
 
 -- wrap in backticks
-vim.keymap.set('v', '<localleader>`', 'c`<c-r>"`')
-vim.keymap.set('n', '<localleader>`', 'viwc`<c-r>"`<esc>')
+vim.keymap.set('v', '<localleader>`', 'c`<c-r>"`', { buffer = 0 })
+vim.keymap.set('n', '<localleader>`', 'viwc`<c-r>"`<esc>', { buffer = 0 })
