a697e3bea8538910afe9a3f192776ad8470c5342
Piero Lescano <me@pierolescano.com>
2026-02-03 20:28:12 -0500

    feat(neovim): Add gitlinker.nvim

--- .config/nvim/lua/plugins/git.lua
+++ .config/nvim/lua/plugins/git.lua
@@ -1,26 +1,33 @@
 return {
-  'lewis6991/gitsigns.nvim',
-  event = { 'BufReadPre', 'BufNewFile' },
-  opts = {
-    preview_config = { border = 'rounded' },
-    on_attach = function(bufnr)
-      local gitsigns = require 'gitsigns'
+  {
+    'lewis6991/gitsigns.nvim',
+    event = { 'BufReadPre', 'BufNewFile' },
+    opts = {
+      preview_config = { border = 'rounded' },
+      on_attach = function(bufnr)
+        local gitsigns = require 'gitsigns'
 
-      -- Navigation
-      vim.keymap.set('n', '[h', function()
-        gitsigns.nav_hunk 'prev'
-      end, { buffer = bufnr, desc = 'Previous [H]unk' })
-      vim.keymap.set('n', ']h', function()
-        gitsigns.nav_hunk 'next'
-      end, { buffer = bufnr, desc = 'Next [H]unk' })
+        -- Navigation
+        vim.keymap.set('n', '[h', function()
+          gitsigns.nav_hunk 'prev'
+        end, { buffer = bufnr, desc = 'Previous [H]unk' })
+        vim.keymap.set('n', ']h', function()
+          gitsigns.nav_hunk 'next'
+        end, { buffer = bufnr, desc = 'Next [H]unk' })
 
-      -- Actions
-      vim.keymap.set({ 'n', 'v' }, '<leader>hs', gitsigns.stage_hunk, { buffer = bufnr })
-      vim.keymap.set({ 'n', 'v' }, '<leader>hr', gitsigns.reset_hunk, { buffer = bufnr })
-      vim.keymap.set('n', '<leader>bs', gitsigns.stage_buffer, { buffer = bufnr })
-      vim.keymap.set('n', '<leader>br', gitsigns.reset_buffer, { buffer = bufnr })
-      vim.keymap.set('n', '<leader>hp', gitsigns.preview_hunk, { buffer = bufnr })
-      vim.keymap.set('n', '<leader>gb', gitsigns.blame, { buffer = bufnr })
-    end,
+        -- Actions
+        vim.keymap.set({ 'n', 'v' }, '<leader>hs', gitsigns.stage_hunk, { buffer = bufnr })
+        vim.keymap.set({ 'n', 'v' }, '<leader>hr', gitsigns.reset_hunk, { buffer = bufnr })
+        vim.keymap.set('n', '<leader>bs', gitsigns.stage_buffer, { buffer = bufnr })
+        vim.keymap.set('n', '<leader>br', gitsigns.reset_buffer, { buffer = bufnr })
+        vim.keymap.set('n', '<leader>hp', gitsigns.preview_hunk, { buffer = bufnr })
+        vim.keymap.set('n', '<leader>gb', gitsigns.blame, { buffer = bufnr })
+      end,
+    },
+  },
+  {
+    'linrongbin16/gitlinker.nvim',
+    cmd = 'GitLink',
+    opts = {},
   },
 }
