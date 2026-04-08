05ba9276c0b643b34234ef13a862f814b1a67c21
Piero Lescano <me@pierolescano.com>
2026-04-07 19:27:12 -0500

    feat(neovim): Add visual mode delete for quickfix

--- .config/nvim/after/ftplugin/qf.lua
+++ .config/nvim/after/ftplugin/qf.lua
@@ -11,3 +11,27 @@ vim.keymap.set('n', 'dd', function()
     vim.fn.cursor(new_line, 1)
   end
 end, { buffer = 0 })
+
+vim.keymap.set('x', 'd', function()
+  local qf_list = vim.fn.getqflist()
+  local start_line = vim.fn.getpos('v')[2]
+  local end_line = vim.fn.getpos('.')[2]
+
+  if start_line > end_line then
+    start_line, end_line = end_line, start_line
+  end
+
+  for _ = start_line, end_line do
+    if qf_list[start_line] then
+      table.remove(qf_list, start_line)
+    end
+  end
+
+  vim.fn.setqflist(qf_list, 'r')
+
+  local new_line = math.min(start_line, #qf_list)
+  vim.fn.cursor(new_line, 1)
+
+  local esc_key = vim.api.nvim_replace_termcodes('<Esc>', true, false, true)
+  vim.api.nvim_feedkeys(esc_key, 'n', false)
+end, { buffer = 0 })
