1bac8f0dadf959dda050e31cabb11d2699737439
Piero Lescano <me@pierolescano.com>
2026-04-14 18:26:33 -0500

    chore(neovim): Remove auto-dark-mode.nvim and simplify color scheme setup
    The plugin is not needed anymore. The theme responds to changes in the
    terminal background.

--- .config/nvim/lua/plugins/ui.lua
+++ .config/nvim/lua/plugins/ui.lua
@@ -4,41 +4,20 @@ return {
     'rebelot/kanagawa.nvim',
     priority = 1000,
     opts = {
-      overrides = function(colors)
-        local theme = colors.theme
+      overrides = function()
         return {
           NormalFloat = { bg = 'none' },
           FloatBorder = { bg = 'none' },
           FloatTitle = { bg = 'none' },
-
-          -- Save an hlgroup with dark background and dimmed foreground
-          -- so that you can use it where your still want darker windows.
-          -- E.g.: autocmd TermOpen * setlocal winhighlight=Normal:NormalDark
-          NormalDark = { fg = theme.ui.fg_dim, bg = theme.ui.bg_m3 },
-
-          -- Popular plugins that open floats will link to NormalFloat by default;
-          -- set their background accordingly if you wish to keep them dark and borderless
-          LazyNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
-          MasonNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
-
-          -- Disable LSP hover highlights
-          LspReferenceTarget = {},
         }
       end,
-      theme = 'dragon',
-      background = {
-        dark = 'dragon',
-        light = 'lotus',
-      },
+      background = { dark = 'dragon', light = 'lotus' },
     },
     config = function(_, opts)
       require('kanagawa').setup(opts)
-      vim.cmd 'colorscheme kanagawa-dragon'
+      vim.cmd 'colorscheme kanagawa'
     end,
   },
-
-  -- Dark and Light Mode
-  { 'f-person/auto-dark-mode.nvim', opts = {} },
 
   -- Tabs
   {
