fa9b97ef71aa025068f48c97274974d1aaf537a2
Piero Lescano <me@pierolescano.com>
2026-07-01 21:09:25 -0500

    feat(nvim): add leetcode.nvim plugin

--- .config/nvim/lua/plugins/extra.lua
+++ .config/nvim/lua/plugins/extra.lua
@@ -13,4 +13,19 @@ return {
 
   -- Ledger
   { 'ledger/vim-ledger', ft = 'ledger' },
+
+  -- LeetCode
+  {
+    'kawre/leetcode.nvim',
+    build = ':TSUpdate html', -- if you have `nvim-treesitter` installed
+    dependencies = {
+      -- include a picker of your choice, see picker section for more details
+      'nvim-lua/plenary.nvim',
+      'MunifTanjim/nui.nvim',
+    },
+    opts = {
+      -- configuration goes here
+      lang = 'golang',
+    },
+  },
 }
